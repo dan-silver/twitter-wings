aba6457746a3c8becef4810397c17d9a6859177d
Daniel Silver <das2c3@mail.missouri.edu>
2014-07-20 20:34:35 -0500

    removed errors

--- express/public/js/tweets.js
+++ express/public/js/tweets.js
@@ -1,7 +1,7 @@
 var phonecatApp = angular.module('twitter-wings', []);
 
 phonecatApp.controller('AppCtrl', function ($scope) {
-  $scope.tweets = [];
+  $scope.twts = [{screen_name: "dan Silver"}];
 
   var socket = io.connect('http://localhost');
   socket.on('twt', function (data) {
@@ -15,7 +15,5 @@ phonecatApp.controller('AppCtrl', function ($scope) {
     $scope.twts.push(data)
     console.log(data);
     $scope.$apply()
-    //  write back to server
-    //  socket.emit('my other event', { my: 'data' });
   });
 });
